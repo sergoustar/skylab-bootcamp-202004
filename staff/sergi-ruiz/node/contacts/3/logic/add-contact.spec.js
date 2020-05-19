5619f683febda80deffd89845e8b88f226a92adc
sergoustar <sergirutu@gmail.com>
2020-05-19 10:10:21 +0200

    manu code 19-5

---
+++ staff/sergi-ruiz/node/contacts/3/logic/add-contact.spec.js
@@ -0,0 +1,23 @@
+const assert = require('assert')
+const addContact = require('./add-contact')
+const { random } = Math
+const fs = require('fs')
+
+{
+    const name = `name-${random()}`
+    const surname = `surname-${random()}`
+    const email = `e-${random()}@mail.com`
+
+    addContact({ name, surname, email }, (error, id) => {
+        assert(error === null)
+
+        assert(typeof id === 'string')
+
+        if (fs.existsSync('../data/${id}')) {
+            console.log('hey');
+        }
+        // TODO check that file <id>.json exists in data folder
+        // TODO read file and check that it contains the name, surname and email
+
+    })
+}
