3025488007831b5b18227e327987d5c98c1e4d27
sergoustar <sergirutu@gmail.com>
2020-05-20 08:52:44 +0200

    server start

--- staff/manuel-barzi/node/contacts/5/index.js
+++ staff/manuel-barzi/node/contacts/5/index.js
@@ -1,7 +1,32 @@
 const net = require('net')
 const listContacts = require('./logic/list-contacts')
+const searchContacts = require('/logic/search-conctacts')
+const http = require('http')
+const url = require('url')
 
 const server = net.createServer(socket => {
+    socket.on('data', data => {
+        searchContacts(query, (error, contacts) => {
+            if (error) throw error
+
+            socket.write(`HTTP/1.1 200
+content-type: text/html
+
+<h2>Contacts list</h2>
+<ul>
+    ${contacts.map(({ name }) => `<li>${name}</li>`).join('')}
+</ul>
+`)
+            socket.end()
+        })
+    })
+
+    socket.on('error', console.log)
+})
+
+server.listen(8080)
+
+/* const server = net.createServer(socket => {
     socket.on('data', data => {
         listContacts((error, contacts) => {
             if (error) throw error
@@ -22,4 +47,4 @@ content-type: text/html
 })
 
 server.listen(8080)
-
+ */
