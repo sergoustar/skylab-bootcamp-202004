339e134a4d337b931bd328d318c69ef2fa54b8bf
sergoustar <sergirutu@gmail.com>
2020-05-21 08:13:15 +0200

    21-5

---
+++ staff/sergi-ruiz/node/contacts/5 copy/index.js
@@ -0,0 +1,62 @@
+const http = require('http')
+const listContacts = require('./logic/list-contacts')
+const searchContacts = require('./logic/search-contacts')
+const ListContacts = require('./components/ListContacts')
+const SearchContacts = require('./components/SearchContacts')
+
+const server = http.createServer((req, res) => {
+    const { url } = req
+
+    res.setHeader('content-type', 'text/html')
+
+    if (url === '/contacts') {
+        listContacts((error, contacts) => {
+            if (error) throw error
+
+            res.end(ListContacts(contacts))
+        })
+    } else if (url.startsWith('/search')) {
+        if (!url.includes('?')) {
+            res.end(SearchContacts())
+        } else {
+            const [, queryString] = url.split('?')
+
+            const [, query] = queryString.split('=')
+
+            searchContacts(query, (error, contacts) => {
+                if (error) throw error
+              
+                res.end(`${SearchContacts(query)}${ListContacts(contacts)}`)
+            })
+        }
+    } else if (url === '/add-contact') {
+
+    } else {
+        
+    }
+})
+
+server.listen(8080)
+
+/* const server = net.createServer(socket => {
+    socket.on('data', data => {
+        listContacts((error, contacts) => {
+            if (error) throw error
+
+            socket.write(`HTTP/1.1 200
+content-type: text/html
+
+<h2>Contacts list</h2>
+<ul>
+    ${contacts.map(({ name }) => `<li>${name}</li>`).join('')}
+</ul>
+`)
+            socket.end()
+        })
+    })
+
+    socket.on('error', console.log)
+})
+
+server.listen(8080)
+ */
